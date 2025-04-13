a7cc1ee4e6c7e235c5b66031383276548c8790be
MinhVi2004 <141652938+MinhVi2004@users.noreply.github.com>
2025-04-13 15:44:29 +0700

    moi nhat

--- front-end/src/Page/TopBar.jsx
+++ front-end/src/Page/TopBar.jsx
@@ -1,8 +1,18 @@
 // TopBar.jsx
 import React from "react";
 import '../CSS/TopBar.css';
+import { Link, useNavigate } from "react-router-dom";
 
 const TopBar = () => {
+     const username = sessionStorage.getItem("username"); // nếu có
+     const [dropdownOpen, setDropdownOpen] = useState(false);
+     const navigate = useNavigate();
+   
+     const handleLogout = () => {
+       sessionStorage.clear();
+       navigate("/signin");
+     };
+  const role = sessionStorage.getItem("userSignin").phanQuyen;
   return (
     <div className="top-bar">
       <div className="logo">Fakebook</div>
@@ -11,7 +21,19 @@ const TopBar = () => {
         <a href="#">About</a>
         <a href="#">Services</a>
         <a href="#">Contact</a>
+        {role === "Quản Trị Viên" && (
+          <Link to="/admin" className="admin-button">Quản lý</Link>
+        )}
       </div>
+      <div className="user-dropdown" onClick={() => setDropdownOpen(!dropdownOpen)}>
+          👤 {username || "User"}
+          {dropdownOpen && (
+            <div className="dropdown-menu">
+              <Link to="/profile">Trang cá nhân</Link>
+              <button onClick={handleLogout}>Đăng xuất</button>
+            </div>
+          )}
+        </div>
     </div>
   );
 };
